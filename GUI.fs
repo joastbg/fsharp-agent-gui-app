2bf4eeafe9f13e62c8ef888622b6e094e4bc8273
Johan Astborg <johan@fsview.se>
2013-05-20 18:10:31 +0200

    First commit

---
+++ GUI.fs
@@ -0,0 +1,38 @@
+﻿
+namespace GUI
+
+    open System
+    open System.Drawing
+    open System.Windows.Forms
+    open Agents
+
+    // User interface form
+    type public SampleForm() as form =
+        inherit Form()
+
+        let valueLabel = new Label(Location=new Point(25,15))
+        let startButton = new Button(Location=new Point(25,50))
+        let sendButton = new Button(Location=new Point(25,75))
+        let agent = MaxAgent.sampleAgent
+
+        let initControls =
+            valueLabel.Text <- "Sample Text"
+            startButton.Text <- "Start"
+            sendButton.Text <- "Send value to agent"
+        do
+            initControls
+
+            form.Controls.Add(valueLabel)
+            form.Controls.Add(startButton)
+
+            form.Text <- "SampleApp F#"
+
+            startButton.Click.AddHandler(new System.EventHandler
+                (fun sender e -> form.eventStartButton_Click(sender, e)))
+
+        // Event handlers
+        member form.eventStartButton_Click(sender:obj, e:EventArgs) =
+            let random = Helpers.genRandomNumber 5
+            Console.WriteLine("Sending value to agent: " + random.ToString())
+            agent.Post(Update random)
+            ()
---
+++ Program.fs
@@ -0,0 +1,18 @@
+﻿/// (C) Johan Astborg, 2013. joastbg@gmail.com
+
+// Main application entry point
+namespace Program
+
+    open System
+    open System.Drawing
+    open System.Windows.Forms
+
+    open GUI
+    
+    module Main =
+        [<STAThread>]
+        do
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(false)
+            let view = new SampleForm()
+            Application.Run(view)
